2a17fb5dde94035b77c07b97432f1b67149d9652
sanchanda <santoshkumar.chanda1@gmail.com>
2026-09-08 14:39:56 +0530

    Add commented config examples to playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,10 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: false
+    // viewport: { width: 1280, height: 720 },
+    // ignoreHTTPSErrors: true,
+    // video: 'on',
+    // screenshot: 'on',
     // screenshot: 'only-on-failure',
     // video: 'retain-on-failure',
 
